942c27a0b5b4f2c485d5510d17bf3d7bc50b7705
TNTree531 <evan.edwards@student.gtchs.org>
2025-02-17 15:49:04 -0500

    working patch before spade added

---
+++ Scripts/camera_2d.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	global_position = get_tree().get_root().get_node('Main/Red_guy').global_position
+	
+	if global_position.y <= -2850:
+		get_tree().get_root().get_node('Main/dawn').visible = false
+		get_tree().get_root().get_node('Main/night').visible = true
+	else:
+		get_tree().get_root().get_node('Main/dawn').visible = true
+		get_tree().get_root().get_node('Main/night').visible = false
--- Scripts/clover.gd
+++ Scripts/clover.gd
@@ -9,11 +9,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if abs(angular_velocity) >= 0.1:
-		get_tree().get_root().get_node('Main/Red_guy').global_position -= (global_position - get_tree().get_root().get_node('Main/Red_guy').global_position)/(get_tree().get_root().get_node('Main/Red_guy').global_position.distance_to(global_position))*abs(angular_velocity)/1.25
-		print(angular_velocity)
-		
-
+	#if abs(angular_velocity) < 25:
+		if abs(angular_velocity) >= 0.1:
+			get_tree().get_root().get_node('Main/Red_guy').global_position -= (global_position - get_tree().get_root().get_node('Main/Red_guy').global_position)/(get_tree().get_root().get_node('Main/Red_guy').global_position.distance_to(global_position))*abs(angular_velocity)/1.25
+			print(angular_velocity)
+	#elif angular_velocity > 25: 
+		#angular_velocity = 25
+	#elif angular_velocity < -25:
+		#angular_velocity = -25
 
 #func _on_clover_area_body_entered(body: Node2D) -> void:
 	#player = body.get_parent().get_node('Red_guy')
