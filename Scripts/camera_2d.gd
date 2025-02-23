d404400c63ed429401d21033019732516a5c1811
TNTree531 <evan.edwards@student.gtchs.org>
2025-02-23 01:00:46 -0500

    Darkness_working

--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -13,7 +13,14 @@ func _physics_process(delta: float) -> void:
 	if Global.active_player == 'purple':
 		global_position = get_tree().get_root().get_node('Main/Purple_guy').global_position
 	
-	if global_position.y <= -2850:
+	if global_position.y <= -6500:
+		get_tree().get_root().get_node('Main/dawn').visible = false
+		get_tree().get_root().get_node('Main/night').visible = false
+		get_tree().get_root().get_node('Main/cave').visible = true
+		get_tree().get_root().get_node('Main/Camera2D/player_light').visible = true
+	elif global_position.y <= -2850:
+		get_tree().get_root().get_node('Main/Camera2D/player_light').visible = false
+		get_tree().get_root().get_node('Main/cave').visible = false
 		get_tree().get_root().get_node('Main/dawn').visible = false
 		get_tree().get_root().get_node('Main/night').visible = true
 	else:
